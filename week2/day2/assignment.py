365b20e6b8bd5f1628397fc99455cb2705ab158b
stoicgit <118203310+stoicgit@users.noreply.github.com>
2022-11-22 15:54:48 -0500

    Create assignment.py
    Ill study hard!

---
+++ week2/day2/assignment.py
@@ -0,0 +1,39 @@
+# 1. Write a function called "nameFunction" have it print out your name. invoke the function
+def NameFunction():
+    print("mauro Stoic")
+NameFunction()
+#2. Define variable named Joe that is the string Joe
+Name = ("Joe")
+#3 Reinitialize Joe so that it is now the integer 4
+Name = (4)
+#4 Change Joe into a float
+Name = (4)
+print(float(Name))
+#5. Create a list named students that includes 5 people from this class
+Students = ["Rokhaya", "Cory", "jessika", "adam", "megan"]
+#5a print the first item from the list(bonus for more than one way)
+print(Students[0])
+#5b print the last item from the list(bonus for more than one way)
+print(Students[-1])
+#5c How do I get the third item from the list?
+print(Students[2])
+#5d print the entire list not in brackets [student1, student2, etc...]
+print(Students[+-1])
+#5e add "is cool" to the end of each name
+Students.append("is cool")
+print(Students)
+#5f if the students name is your name add "IT ME!!" to the end
+#skip
+#5g if you are not in the list print "I guess I'm not cool"
+print("I guess I'm not cool")
+#5h list some methods to remove things from a list
+## myList = pop(), reomove(), del()
+#6 Create a dictionary with keys "Digital Crafts" ,"Instructor", "TA" and values "Bootcamp" ,"Joe" ,"Ethan"
+School={
+    "digital craft": "Instructors",
+    "TA" "Bootcamp": {"Joe", "Ethan"}
+}
+print(School)
+#7 Write a class named Cars with attributes make,model,year, and type(sedan,truck,crossover, sportscar, etc....)
+class Cars:
+    def __init__ (self, make, model, year)
