edf2ef57c23cf1a36121c6bd950ff4fed8c19e8a
stoicgit <118203310+stoicgit@users.noreply.github.com>
2022-11-28 10:31:00 -0500

    project attempt.1
    project

---
+++ week2/day3/project attempt1.py
@@ -0,0 +1,108 @@
+print("""
+                                                            
+Rainbow                     88          88 88              
+                            88          88 ""              
+                            88          88                 
+     ,adPPYb,d8  ,adPPYba,  88,dPPYba,  88 88 8b,dPPYba,   
+    a8"    `Y88 a8"     "8a 88P'    "8a 88 88 88P'   `"8a  
+    8b       88 8b       d8 88       d8 88 88 88       88  
+    "8a,   ,d88 "8a,   ,a8" 88b,   ,a8" 88 88 88       88  
+     `"YbbdP"Y8  `"YbbdP"'  8Y"Ybbd8"'  88 88 88       88  
+     aa,    ,88                                            
+      "Y8bbdP"                                             
+      """)
+userChoice = ""
+
+userChoice = input("what is your hero's name?")
+
+userChoice = input("veiw map? y/n")
+
+def map():
+    print("""
+    1. go to gray_forest
+    2. go to gray_valley  
+    3. go to gray_cliff
+          """)
+
+def gray_forest():
+    print("""
+    1. fight red goblin
+    2. fight orange goblin
+    3. fight yellow goblin
+    4. run away   
+           """)
+    
+def gray_valley():
+    print(""""
+    1. fight green goblin
+    2. fight blue goblin
+    3. fight indigo goblin
+    4. run away
+          """)
+
+def gray_cliff():
+    print(""""
+    1. fight the violet goblin
+    2. run away     
+          """)
+
+class Hero:
+    name = ""
+    def __init__(self, attack, health, defense):
+        self.attack = 10
+        self.health = 10
+        self.defense = 8
+class RedGoblin:
+    def __init__(self, attack, health, defense):
+        self.attack = 5
+        self. health = 5
+        self.defense = 6
+class OrangeGoblin:
+      def __init__(self, attack, health, defense):
+        self.attack = 5
+        self. health = 5
+        self.defense = 6
+class YellowGoblin:
+    def __init__(self, attack, health, defense):
+        self.attack = 5
+        self. health = 5
+        self.defense = 6
+class GreenGoblin:
+    def __init__(self, attack, health, defense):
+        self.attack = 5
+        self. health = 5
+        self.defense = 6
+class BlueGoblin:
+    def __init__(self, attack, health, defense):
+        self.attack = 5
+        self. health = 5
+        self.defense = 6
+class IndigoGoblin:
+    def __init__(self, attack, health, defense):
+        self.attack = 5
+        self. health = 5
+        self.defense = 6
+class VioletGoblin:
+    def __init__(self, attack, health, defense):
+        self.attack = 5
+        self. health = 5
+        self.defense = 6
+    
+while True:
+    if(userChoice == "n"):
+        break
+    if(userChoice == "y"):
+        map()
+        break
+
+userChoice = input("where would you like to go?")
+while True:
+    if(userChoice == "1"):
+        gray_forest()
+        break
+    if(userChoice == "2"):
+        gray_valley()
+        break
+    if(userChoice == "3"):
+        gray_cliff()
+        break
---
+++ week2/day3/review.py
@@ -0,0 +1,42 @@
+# variables
+racoons = "Trash panda"
+toyota = "car"
+print(racoons)
+
+print("racoons")
+
+print(racoons + toyota)
+
+computers = {"dell" : "trash"}
+print(computers["dell"])
+
+computer["mac"] = "overpriced best compter"
+print(computers)
+
+# list
+colors = ["blue", "red", "white"]
+print(colors[1])
+colors.append("green")
+# modify last in list
+# colors[-1] = "green"
+
+print(colors)
+# if statments
+# if(condition):
+    # code
+counter = 5
+if(counter ==5)
+    print("counter has reached 5")
+
+
+
+# funtions
+
+
+    
+
+
+
+# fuction
+
+# while loop
