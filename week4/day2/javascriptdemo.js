feaa8874588453c0c5f97fdd33805547d1d8c771
stoicgit <118203310+stoicgit@users.noreply.github.com>
2022-12-06 14:07:30 -0500

    java

---
+++ week4/day2/javascriptdemo.js
@@ -0,0 +1,45 @@
+// print()
+console.log("hello world");
+
+// datatypes
+//type of data
+
+//strings
+//python name = joe
+//let,var,const
+
+//let is used todefine a variablethat you can reassign
+let houston = hot
+console.log(houston)
+houston = 2;
+console.log(houston);
+
+//const but it stands for CONSTANT, it can only be what you define it
+const ethan = "a cool dude"
+console.log(houston);
+ethan = "not a cool guy"
+
+//functions
+
+//def is how you wrote  it in python, in javascript we have two ways
+
+function printmyname(){
+// this is the function body
+//it is any code inside of the curly braces
+    console.log("joe");
+}
+printmyname();
+
+
+
+
+
+
+
+
+
+
+
+
+// arrays
+let fruit = []
