dcb91600d68699fcd9aa5bb9302ded93035c17c2
stoicgit <118203310+stoicgit@users.noreply.github.com>
2022-12-16 10:37:43 -0500

    update
    update

--- week5/day4/wap.js
+++ week5/day4/wap.js
@@ -4,7 +4,7 @@ const getWeather = async () => {
     const weatherInput = document.querySelector(".weather").value;
     console.log(weatherInput);
     const weatherData = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${weatherInput}&appid=ff1cadec65d9f10c3aafc2a01b14612c`
+    
 );
     const json = await weatherData.json();
     const currentCity = document.getElementById("current-city")
